5b6d48cd38dd43f8903ebf8a0980450a10f710a1
tentaswole <tliu07029@gmail.com>
2024-05-25 15:44:13 -0700

    update schedule

--- src/App.tsx
+++ src/App.tsx
@@ -1,109 +1,44 @@
-import { useMemo, useState } from 'react'
-import { FirebaseApp, initializeApp } from 'firebase/app'
-import { getDatabase, ref, child, get, DatabaseReference } from "firebase/database";
-import Rsvp from './Rsvp.tsx'
+import { useState } from 'react'
 import './App.css'
+import Schedule from './Schedule'
 
-enum Tabs {
+export enum Tab {
   HOME = 'HOME',
-  SCHEDULE = 'SCHEDULE',
+  SCHEDULE = 'SCHEDULE', 
   DRESS_CODE = 'DRESS_CODE',
-  FAQ = 'FAQ',
-  RSVP = 'RSVP'
-}
-
-export type GuestData = {
-  names: string[];
-  rsvp?: RsvpData[]
-}
-
-export type RsvpData = {
-  attending: boolean;
-  dietaryRestrictions: string;
-}
+  FAQ = 'FAQ'
+} 
 
 function App() {
-  const [tab, setTab] = useState<Tabs>(Tabs.HOME);
-  const [passcode, setPasscode] = useState<string>("");
-  const [passcodeData, setPasscodeData] = useState<GuestData>();
-  let app: FirebaseApp;
-  let dbRef: DatabaseReference;
+  const [tab, setTab] = useState<Tab>(Tab.HOME);
 
-  [app, dbRef] = useMemo(() => {
-    const config = {
-      apiKey: "AIzaSyB0NBRt5oi1WcOLX5njGL1E3m1VSYT_ZF0",
-      authDomain: "tara-and-loren.firebaseapp.com",
-      databaseURL: "https://tara-and-loren-default-rtdb.firebaseio.com",
-      projectId: "tara-and-loren",
-      storageBucket: "tara-and-loren.appspot.com",
-      messagingSenderId: "895589907035",
-      appId: "1:895589907035:web:093b39bcd6cd2493c09d7e"
-    };
-    app = initializeApp(config);
-    dbRef = ref(getDatabase(app));
-    return [app, dbRef];
-  }, [])
 
-  const getContent = (tab: Tabs) => {
+  const getContent = (tab: Tab) => {
     switch (tab) {
-      case Tabs.HOME:
-        return <h2>Welcome, {passcodeData?.names.join(", ")}!</h2>;
-      case Tabs.SCHEDULE:
-        return <p>The wedding starts at 5pm ? </p>;
-      case Tabs.DRESS_CODE:
+      case Tab.HOME:
+        return <p>Welcome home!</p>;
+      case Tab.SCHEDULE:
+        return <Schedule/>;
+      case Tab.DRESS_CODE: 
         return <p>You are dressing up for the park </p>;
-      case Tabs.FAQ:
-        return <p>No one has asked me a question yet. </p>;
-      case Tabs.RSVP:
-        return passcodeData == undefined ? null : <Rsvp guestData={passcodeData} passcode={passcode} />;
+      case Tab.FAQ: 
+       return <p>No one has asked me a question yet! </p>;
     }
   }
 
-  const getHeading = () => <>
-    <div className='titleContainer'><h1>Tara & Loren</h1></div>
-    <img className='weddingBorder' src='https://raw.githubusercontent.com/shermansplanet/wedding/main/src/assets/wedding-border.png'></img>
-  </>;
-
-  if (passcodeData == undefined) {
-    return (
-      <>
-        {getHeading()}
-        <div className='navbar' style={{ height: "20px" }} />
-        <form className='passcodeContainer' onSubmit={(event) => {
-          event.preventDefault();
-          return false;
-        }}>
-          <input value={passcode} placeholder='passcode' onChange={(e) => setPasscode(e.target.value.toLowerCase())}></input>
-          <div style={{ height: 16 }} />
-          <button onClick={() => {
-            get(child(dbRef, `passcodes/` + passcode)).then((snapshot) => {
-              if (snapshot.exists()) {
-                setPasscodeData(snapshot.val())
-              } else {
-                console.log("No data available");
-              }
-            }).catch((error) => {
-              console.error(error);
-            });
-          }}>Submit</button>
-        </form>
-      </>
-    )
-  }
-
   return (
-    <>
-      {getHeading()}
-      <div className='navbar'>
-        <button style={{ border: 'none', background: 'none', }} onClick={() => setTab(Tabs.RSVP)}>RSVP</button>
-        <button style={{ border: 'none', background: 'none', }} onClick={() => setTab(Tabs.SCHEDULE)}>Schedule</button>
-        <button style={{ border: 'none', background: 'none', }} onClick={() => setTab(Tabs.DRESS_CODE)}>Dress Code</button>
-        <button style={{ border: 'none', background: 'none', }} onClick={() => setTab(Tabs.FAQ)}>FAQ</button>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', margin: 0}}>
+      <h1>Tara & Loren</h1>
+      <div style={{display: 'flex', gap: 16}}>
+        <button onClick={() => setTab(Tab.SCHEDULE)}>Schedule</button>
+        <button onClick={() => setTab(Tab.DRESS_CODE)}>Dress Code</button>
+        <button onClick={() => setTab(Tab.FAQ)}>FAQ</button>
       </div>
-      <div className="card">
-        {getContent(tab)}
-      </div>
-    </>
+      {getContent(tab)}
+      <p style={{fontSize: '.7em'}}>
+        copyright by Tara & Loren 2024
+      </p>
+    </div>
   )
 }
 
---
+++ src/Schedule.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function Schedule() {
+  return (
+    <div style={{display: 'flex', flexDirection: 'column', gap: 16}}>
+      <h2>Tentative Wedding Schedule</h2>
+      <p>August 24, 2024</p>
+      <p>Stable Cafe </p>
+      <p>2128 Folsom St, San Francisco, CA 94110</p>
+      <div style={{display: 'flex', justifyContent:'space-between'}}><p>4:30pm</p> <p>Doors open</p></div>
+      <div style={{display: 'flex', justifyContent:'space-between'}}><p>4:45pm</p> <p>(Optional) Flower Crown Making</p></div>
+      <div style={{display: 'flex', justifyContent:'space-between'}}><p>5:30pm</p> <p>Ceremony</p></div>
+      <div style={{display: 'flex', justifyContent:'space-between'}}><p>6:00pm</p> <p>Social Hour</p></div>
+      <div style={{display: 'flex', justifyContent:'space-between'}}><p>7:00pm</p> <p>Dinner</p></div>
+      <div style={{display: 'flex', justifyContent:'space-between'}}><p>8:00pm</p> <p>Music, Dancing, Games</p></div>
+      <div style={{display: 'flex', justifyContent:'space-between'}}><p>10:00pm</p> <p>Last Song</p></div>
+      </div>  )
+}
+
+export default Schedule
