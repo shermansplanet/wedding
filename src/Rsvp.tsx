166bbaa399d662ec4ea0052feae0ec454d20bd26
shermansplanet <lorenzosherman@gmail.com>
2024-05-25 15:48:39 -0700

    LARP menu option

--- src/Rsvp.tsx
+++ src/Rsvp.tsx
@@ -17,22 +17,32 @@ const Rsvp = ({ guestData, passcode }: RsvpProps) => {
     for (let i = 0; i < guestCount; i++) {
         const ci = i;
         if (rsvps[i] == undefined) {
-            rsvps[i] = { attending: false, dietaryRestrictions: "" };
+            rsvps[i] = { attending: undefined, dietaryRestrictions: "" };
         }
         let rsvp = rsvps[i];
         rsvpElements.push(<div style={{ marginBottom: "24px" }} key={"rsvp_" + i}>
             <h2>RSVP for {guestData.names[i]}</h2>
-            <div className='checkboxRow'><button
-                className={rsvp.attending ? "checkmarkButton checked" : "checkmarkButton"}
-                onClick={() => {
-                    rsvp.attending = !rsvp.attending;
-                    rsvps[ci] = rsvp;
-                    setRsvps({ ...rsvps });
-                }}>
-            </button>I can attend the wedding.</div>
+            <div className='checkboxRow'>
+                <button
+                    className={rsvp.attending == true ? "checkmarkButton checked" : "checkmarkButton"}
+                    onClick={() => {
+                        rsvp.attending = true;
+                        rsvps[ci] = rsvp;
+                        setRsvps({ ...rsvps });
+                    }}>I can attend
+                </button>
+                <button
+                    className={rsvp.attending == false ? "checkmarkButton checked" : "checkmarkButton"}
+                    onClick={() => {
+                        rsvp.attending = false;
+                        rsvps[ci] = rsvp;
+                        setRsvps({ ...rsvps });
+                    }}>I cannot attend
+                </button>
+            </div>
             <input style={{ marginTop: "4px", padding: "4px 0px", fontSize: "16pt" }}
                 placeholder='Dietary Restrictions'
-                content={rsvp.dietaryRestrictions}
+                value={rsvp.dietaryRestrictions}
                 onChange={e => {
                     rsvp.dietaryRestrictions = e.target.value;
                     rsvps[ci] = rsvp;
